9510ef2f91c687138402bff4897f1481611bc75e
Muexx <stephan.muekusch@gmail.com>
2021-04-23 09:50:51 +0200

    update

--- lib/ublox/ublox.h
+++ lib/ublox/ublox.h
@@ -2,12 +2,13 @@
 #define UBLOX_H
 
 #include <Arduino.h>
-
+#include <circularbuffer.h>
 
 class UBlox
 {
 private:
     const uint8_t i2c_address {0x42};
+    CircularBuffer rx_rtcm_buffer(300);
 public:
     uint8_t send(u_int8_t *data, u_int8_t len);
     UBlox();
---
+++ src/config.h
@@ -0,0 +1,8 @@
+
+namespace config{
+    const int REGULAR_CHECK_INTERVAL = 300;
+    const int CONNECTION_TIMEOUT = 1000;
+    const int SEND_BUFFER_SIZE = 600;
+    const int RTCM_BUFFER_SIZE = 300;
+}
+
